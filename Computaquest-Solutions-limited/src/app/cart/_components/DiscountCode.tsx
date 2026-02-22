bb837239ca3acf2e7743ec74753300d008535d03
unknown <charleskeyz2017@gmail.com>
2026-02-22 10:45:17 +0100

    commit code

--- Computaquest-Solutions-limited/src/app/cart/_components/DiscountCode.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import CheckoutSummarySection from "@src/components/PageFragments/CheckoutSummarySection";
-import React from "react";
-
-const DiscountCode = () => {
-	return <CheckoutSummarySection />;
-};
-
-export default DiscountCode;
